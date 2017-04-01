cbd2295035c682b284007a7b6ea6e250082d5a6b
abdoutelb <abdoutelb.fci@gmail.com>
2017-04-02 01:21:36 +0200

    Adding Logic to the new version

--- some-project/index.js
+++
@@ -1,7 +0,0 @@
-const {sensitiveWords} = require('sensitive-word')
-
-const filtered = sensitiveWords(
-    'The new apple makbook pro will have a touchbar',
-    ['pro','touchbar']
-)
-console.log(filtered)
